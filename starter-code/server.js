b76df93ea55edab516f9375b481897690ab2c938
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-07-19 20:46:44 -0700

    TODO DONE

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// TODO: Install and require the NPM Postgres package 'pg' into your server.js, and ensure that it is then listed as a dependency in your package.json
+// Done: Install and require the NPM Postgres package 'pg' into your server.js, and ensure that it is then listed as a dependency in your package.json
+const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
 
@@ -19,7 +20,7 @@ const conString = 'postgres://localhost:5432/kilavolt';
 //       This is how it knows the URL and, for Windows and Linux users, our username and password for our
 //       database when client.connect is called on line 26. Thus, we need to pass our conString into our
 //       pg.Client() call.
-const client = new pg.Client('something needs to go here... read the instructions above!');
+const client = new pg.Client(conString);
 
 // REVIEW: Use the client object to connect to our DB.
 client.connect();
@@ -27,7 +28,9 @@ client.connect();
 
 // REVIEW: Install the middleware plugins so that our app is aware and can use the body-parser module
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('./public'));
 
 
@@ -35,7 +38,9 @@ app.use(express.static('./public'));
 app.get('/new', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // Put your response here...
-  response.sendFile('new.html', {root: './public'});
+  response.sendFile('new.html', {
+    root: './public'
+  });
 });
 
 
@@ -44,94 +49,91 @@ app.get('/articles', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // Put your response here...
   client.query('SELECT * FROM articles')
-  .then(function(result) {
-    response.send(result.rows);
-  })
-  .catch(function(err) {
-    console.error(err)
-  })
+    .then(function(result) {
+      response.send(result.rows);
+    })
+    .catch(function(err) {
+      console.error(err)
+    })
 });
 
 app.post('/articles', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // Put your response here...
   client.query(
-    `
+      `
       INSERT INTO
         articles(title, author, "authorUrl", category, "publishedOn", body)
       VALUES ($1, $2, $3, $4, $5, $6);
-    `,
-    [
-      request.body.title,
-      request.body.author,
-      request.body.authorUrl,
-      request.body.category,
-      request.body.publishedOn,
-      request.body.body
-    ]
-  )
-  .then(function() {
-    response.send('insert complete')
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+    `, [
+        request.body.title,
+        request.body.author,
+        request.body.authorUrl,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body
+      ]
+    )
+    .then(function() {
+      response.send('insert complete')
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 });
 
 app.put('/articles/:id', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // Put your response here...
   client.query(
-    `UPDATE articles
+      `UPDATE articles
     SET
       title=$1, author=$2, "authorUrl"=$3, category=$4, "publishedOn"=$5, body=$6
     WHERE article_id=$7;
-    `,
-    [
-      request.body.title,
-      request.body.author,
-      request.body.authorUrl,
-      request.body.category,
-      request.body.publishedOn,
-      request.body.body,
-      request.params.id
-    ]
-  )
-  .then(function() {
-    response.send('update complete')
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+    `, [
+        request.body.title,
+        request.body.author,
+        request.body.authorUrl,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body,
+        request.params.id
+      ]
+    )
+    .then(function() {
+      response.send('update complete')
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 });
 
 app.delete('/articles/:id', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // Put your response here...
   client.query(
-    `DELETE FROM articles WHERE article_id=$1;`,
-    [request.params.id]
-  )
-  .then(function() {
-    response.send('Delete complete')
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+      `DELETE FROM articles WHERE article_id=$1;`, [request.params.id]
+    )
+    .then(function() {
+      response.send('Delete complete')
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 });
 
 app.delete('/articles', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // Put your response here...
   client.query(
-    'DELETE FROM articles;'
-  )
-  .then(function() {
-    response.send('Delete complete')
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+      'DELETE FROM articles;'
+    )
+    .then(function() {
+      response.send('Delete complete')
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 });
 
 
@@ -152,27 +154,25 @@ function loadArticles() {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // Put your response here...
   client.query('SELECT COUNT(*) FROM articles')
-  .then(function(result) {
-    // REVIEW: result.rows is an array of objects that Postgres returns as a response to a query.
-    //         If there is nothing on the table, then result.rows[0] will be undefined, which will
-    //         make count undefined. parseInt(undefined) returns NaN. !NaN evaluates to true.
-    //         Therefore, if there is nothing on the table, line 151 will evaluate to true and
-    //         enter into the code block.
-    if(!parseInt(result.rows[0].count)) {
-      fs.readFile('./public/data/hackerIpsum.json', function(err, fd) {
-        if(err) console.error(err)
-        JSON.parse(fd.toString()).forEach(function(ele) {
-          client.query(`
+    .then(function(result) {
+      // REVIEW: result.rows is an array of objects that Postgres returns as a response to a query.
+      //         If there is nothing on the table, then result.rows[0] will be undefined, which will
+      //         make count undefined. parseInt(undefined) returns NaN. !NaN evaluates to true.
+      //         Therefore, if there is nothing on the table, line 151 will evaluate to true and
+      //         enter into the code block.
+      if (!parseInt(result.rows[0].count)) {
+        fs.readFile('./public/data/hackerIpsum.json', function(err, fd) {
+          if (err) console.error(err)
+          JSON.parse(fd.toString()).forEach(function(ele) {
+            client.query(`
             INSERT INTO
             articles(title, author, "authorUrl", category, "publishedOn", body)
             VALUES ($1, $2, $3, $4, $5, $6);
-          `,
-            [ele.title, ele.author, ele.authorUrl, ele.category, ele.publishedOn, ele.body]
-          )
+          `, [ele.title, ele.author, ele.authorUrl, ele.category, ele.publishedOn, ele.body])
+          })
         })
-      })
-    }
-  })
+      }
+    })
 }
 
 function loadDB() {
@@ -186,13 +186,11 @@ function loadDB() {
       "authorUrl" VARCHAR (255),
       category VARCHAR(20),
       "publishedOn" DATE,
-      body TEXT NOT NULL);`
-    )
+      body TEXT NOT NULL);`)
     .then(function() {
       loadArticles();
     })
     .catch(function(err) {
       console.error(err);
-    }
-  );
+    });
 }
